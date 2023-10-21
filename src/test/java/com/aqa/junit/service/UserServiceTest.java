707829044ba4085e9965251c40984fe51b422620
aqaacc <mralextb@gmail.com>
2023-10-21 09:28:49 +0200

    added 2nd Test + annotations TestInstance, BeforeAll, AfterAll,

--- src/test/java/com/aqa/junit/service/UserServiceTest.java
+++ src/test/java/com/aqa/junit/service/UserServiceTest.java
@@ -1,15 +1,45 @@
 package com.aqa.junit.service;
 
-import org.junit.jupiter.api.Assertions;
+import com.aqa.junit.dto.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
+
+    private UserService userService;
+
+    @BeforeAll
+    void init(){
+        System.out.println("Before all " + this);
+    }
 
     @Test
     void userEmptyIfNoUserAdded(){
-        var userService = new UserService();
+        userService = new UserService();
         var users =  userService.getAll();
-        Assertions.assertTrue(users.isEmpty());
+        assertTrue(users.isEmpty());
+        System.out.println("Success 1 " + this);
+    }
 
+    @Test
+    void userSizeIfUserAdded(){
+        userService = new UserService();
+        userService.add(new User());
+        userService.add(new User());
+
+        var userServiceAll = userService.getAll();
+        assertEquals(2, userServiceAll.size());
+        System.out.println("Success 2 " + this);
+    }
+
+    @AfterAll
+    void closeResources(){
+        System.out.println("After all " + this);
     }
 }
