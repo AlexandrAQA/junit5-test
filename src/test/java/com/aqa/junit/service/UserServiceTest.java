ce61d2cc8e9c17b2a250dc5dc917e563082e2e7d
aqaacc <mralextb@gmail.com>
2023-11-02 22:10:28 +0100

    big refactoring of all classes

--- src/test/java/com/aqa/junit/service/UserServiceTest.java
+++ src/test/java/com/aqa/junit/service/UserServiceTest.java
@@ -1,78 +1,110 @@
 package com.aqa.junit.service;
 
 import com.aqa.junit.dto.User;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsMapContaining;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+import java.util.Map;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class UserServiceTest {
+@TestInstance(Lifecycle.PER_CLASS)
+class UserServiceTest {
 
-    private static final User MARK = User.of(1, "Mark", "111212121");
-    private static final User MAX = User.of(2, "Max", "2221212121");
-    private final UserService userService = new UserService();
+    private static final User IVAN = User.of(1, "Ivan", "123");
+    private static final User PETR = User.of(2, "Petr", "111");
+
+    private UserService userService;
 
     @BeforeAll
-    void init(){
-        System.out.println("Before all " + this);
+    void init() {
+        System.out.println("Before all: " + this);
+    }
+
+    @BeforeEach
+    void prepare() {
+        System.out.println("Before each: " + this);
+        userService = new UserService();
     }
 
     @Test
-    void userEmptyIfNoUserAdded(){
-        var users =  userService.getAll();
-        assertTrue(users.isEmpty());
-        System.out.println("Success 1 " + this);
+    void usersEmptyIfNoUserAdded() {
+        System.out.println("Test 1: " + this);
+        var users = userService.getAll();
+
+        MatcherAssert.assertThat(users, empty());
+        assertTrue(users.isEmpty(), () -> "User list should be empty");
+//        input -> [box == func] -> actual output
     }
 
     @Test
-    void userSizeIfUserAdded(){
-        userService.add(MARK);
-        userService.add(MAX);
-        var userServiceAll = userService.getAll();
-        assertEquals(4, userServiceAll.size());
-        System.out.println("Success 2 " + this);
+    void usersSizeIfUserAdded() {
+        System.out.println("Test 2: " + this);
+        userService.add(IVAN);
+        userService.add(PETR);
+
+        var users = userService.getAll();
+
+        assertThat(users).hasSize(2);
+//        assertEquals(2, users.size());
     }
 
     @Test
-    void loginSuccessIfUserExists(){
-        userService.add(MARK);
-        Optional<User> userOptional = userService.login(MARK.getUsername(), MARK.getPassword());
-        assertTrue(userOptional.isPresent());
-        //Assertions.assertEquals(userOptional, MARK);
+    void loginSuccessIfUserExists() {
+        userService.add(IVAN);
 
+        Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+
+        assertThat(maybeUser).isPresent();
+        maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
+//        assertTrue(maybeUser.isPresent());
+//        maybeUser.ifPresent(user -> assertEquals(IVAN, user));
     }
 
     @Test
-    void loginFailedIfUserIsNotCorrect(){
-        userService.add(MAX);
-        var maybeUser = userService.login(MAX.getUsername(), "ssss");
+    void usersConvertedToMapById() {
+        userService.add(IVAN, PETR);
+
+        Map<Integer, User> users = userService.getAllConvertedById();
+
+        MatcherAssert.assertThat(users, IsMapContaining.hasKey(IVAN.getId()));
+        assertAll(
+                () -> assertThat(users).containsKeys(IVAN.getId(), PETR.getId()),
+                () -> assertThat(users).containsValues(IVAN, PETR)
+        );
+    }
+
+    @Test
+    void loginFailIfPasswordIsNotCorrect() {
+        userService.add(IVAN);
+
+        var maybeUser = userService.login(IVAN.getUsername(), "dummy");
+
         assertTrue(maybeUser.isEmpty());
-
     }
 
     @Test
-    void loginFailedIfUserDoesNotExist(){
-        userService.add(MAX);
-        var maybeUser = userService.login("invalidUser", MARK.getPassword());
+    void loginFailIfUserDoesNotExist() {
+        userService.add(IVAN);
+
+        var maybeUser = userService.login("dummy", IVAN.getPassword());
+
         assertTrue(maybeUser.isEmpty());
     }
 
-    @Test
-    void usersSizeAssertJ(){
-        System.out.println("Assert J Test");
-        userService.add(MAX);
-        userService.add(MARK);
-        var usersAssertJ = userService.getAll();
-        assertThat(usersAssertJ).hasSize(2);
-
+    @AfterEach
+    void deleteDataFromDatabase() {
+        System.out.println("After each: " + this);
     }
 
     @AfterAll
-    void closeResources(){
-        System.out.println("After all " + this);
+    void closeConnectionPool() {
+        System.out.println("After all: " + this);
     }
 }
